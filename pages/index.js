2d3e725d91935a710323e6ea0f352f9d1622e731
Jeanette Schwarz <jeanne.schwarz1306@gmail.com>
2024-02-28 12:30:21 +0100

    link

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import PlaceCard from "@/components/PlaceCard";
 import Link from "next/link";
+import { MapLink } from "@/components/DetailsCard";
 
 export default function HomePage({ initialPlaces }) {
   return (
     <div>
-      <Link href="/create"> Add a new Place</Link>
+      <MapLink href="/create"> Add a new Place</MapLink>
       <StyledUl>
         {initialPlaces.map(({ id, name, location, image }) => (
           <PlaceCard key={id} name={name} location={location} image={image} />
